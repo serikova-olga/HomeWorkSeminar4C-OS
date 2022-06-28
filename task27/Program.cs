6acc6d7ed0000773585666fa5b786ef7b6db43c2
OLGA <serikova-olga@mail.ru>
2022-06-28 16:10:55 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Write("input number: ");
+int n = Convert.ToInt32(Console.ReadLine()) ;
+
+int SumDigit(int sum) 
+{
+    sum = n%10 + n/100 + n%100/10;
+    return sum;
+}
+
+Console.Write(SumDigit (n));
